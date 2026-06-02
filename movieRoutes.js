5ad4594bc29923daac709446a7438e6a6cbb35a9
Julia-dev01 <julia.paim@aluno.senai.br>
2026-06-02 17:30:08 -0300

    Delete movieRoutes.js

--- movieRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getMovies, createMovie } from '../controllers/movieController.js';
-
-const router = Router();
-
-router.get('/movies', getMovies);
-router.post('/movies', createMovie);
-
-export default router;
